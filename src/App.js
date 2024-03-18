398f70ec04023f41f6e37d475bb7a5df579dab12
YunHyeRim1 <0305_08@naver.com>
2024-03-18 23:16:04 +0900

    버튼 기능개발과 state변경하기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,7 +7,8 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
   
   return (
     <div className="App">
@@ -13,8 +16,9 @@ function App() {
         <div>ReactBlog</div>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
+        <button onClick={()=>{ 글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬독학']) }}>수정버튼</button>
       </div>
       <div className="list">
         <h4>{글제목[1]}</h4>
