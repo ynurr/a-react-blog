0dbfbbf62e6121ef5e2831cd61559e5c923d6534
YunHyeRim1 <0305_08@naver.com>
2024-03-21 00:02:26 +0900

    동적인 UI 만들기 (모달창)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
   
   return (
     <div className="App">
@@ -37,11 +38,13 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{ modal == true ? setModal(false) : setModal(true) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <Modal></Modal>
+      {
+        modal == true ? <Modal/> : null
+      }
       
     </div>
   );
