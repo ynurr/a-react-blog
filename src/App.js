8f7a241b957a49ce0bfa0230214cfd76f1a5dca5
YunHyeRim1 <0305_08@naver.com>
2024-03-24 21:58:03 +0900

    props 글제목 적용하기

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
   
   return (
     <div className="App">
@@ -45,8 +46,8 @@ function App() {
       {
         글제목.map(function(a, i){
           return (
-            <div className="list" key={i}>
-              <h4 onClick={()=>{ setModal(true) }}>{ a } 
+            <div className="list" >
+              <h4 onClick={()=>{ setModal(true); setTitle(i) }}>{ a } 
                 <span onClick={()=>{
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
@@ -60,7 +61,7 @@ function App() {
       }
 
       {
-        modal == true ? <Modal 글제목변경={글제목변경} color="#d7dcf5" 글제목={글제목}/> : null
+        modal == true ? <Modal title={title} 글제목변경={글제목변경} color="#d7dcf5" 글제목={글제목}/> : null
       }
       
     </div>
@@ -70,7 +71,7 @@ function App() {
 function Modal(props) { // 다른 function 밖에 만들어야 한다.
   return ( // retrun (안에서는 하나의 태그로 시작해서 하나의 태그로 끝나야 한다.)
     <div className="modal" style={{background : props.color}}>
-      <h4>{props.글제목[0]}</h4>
+      <h4>{ props.글제목[props.title] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button onClick={()=>{ props.글제목변경(['여자코트 추천','강남 우동맛집','파이썬독학']) }}>글수정</button>
